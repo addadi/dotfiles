662c79323414d9fd4865a408aa6b0169ddb79d3f
Guy Addadi <addadi@gmail.com>
2026-04-14 16:21:32 +0200

    fix(markdown-preview): enable pre-built binary install
    Uncomment the recommended build command that downloads pre-built
    binaries instead of requiring yarn/npm.

--- init.lua
+++ init.lua
@@ -1190,7 +1190,7 @@ require("lazy").setup(
         --  Markdown
         {
             "iamcco/markdown-preview.nvim",
-            --build = function() vim.fn["mkdp#util#install"]() end,
+            build = function() vim.fn["mkdp#util#install"]() end,
             ft = "markdown",
             keys = { { "<A-o>", "<CMD>MarkdownPreviewToggle<CR>", desc = "Toggle Markdown Preview" } }
         },
