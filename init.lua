6d808e278be82cfbb569bd26b2c73e515f163869
Guy Addadi <addadi@gmail.com>
2026-04-14 16:58:16 +0200

    chore: add pyrepl.nvim as TODO candidate, clean up stale comments
    Add commented-out pyrepl.nvim spec for future evaluation as a
    molten-nvim replacement. Remove stale comments from molten/jupytext
    specs.

--- init.lua
+++ init.lua
@@ -1605,19 +1605,20 @@ require("lazy").setup(
         --NOTEBOOK SUPPORT
         {
             "benlubas/molten-nvim",
-            version = "^1.0.0", -- use version <2.0.0 to avoid breaking changes
+            version = "^1.0.0",
             build = ":UpdateRemotePlugins",
             init = function()
-                -- this is an example, not a default. Please see the readme for more configuration options
                 vim.g.molten_output_win_max_height = 12
             end,
         },
         {
             "GCBallesteros/jupytext.nvim",
             config = true,
-            -- Depending on your nvim distro or config you may need to make the loading not lazy
-            -- lazy=false,
         },
+        -- TODO: evaluate pyrepl.nvim as molten-nvim replacement
+        -- pyrepl.nvim is a newer, simpler Jupyter integration that uses
+        -- jupyter-console directly. Worth testing if molten feels heavy.
+        -- { "ADB-Geeks/pyrepl.nvim", opts = {} },
     }
 )
 
